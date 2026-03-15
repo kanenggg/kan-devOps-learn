56df3026e9add0795e8e7736071959e17d2b3f48
kanenggg <ganfyeiei197@gmail.com>
2026-03-15 22:26:06 +0700

    printLove3000

--- Lab0/index.js
+++ Lab0/index.js
@@ -3,7 +3,7 @@ import express from "express"
 const app = express()
 
 app.get("/", (req, res) => {
-  res.send("i love 3000")
+  res.send("I Love You 3000")
 })
 
 app.listen(3000, "0.0.0.0", () => {
